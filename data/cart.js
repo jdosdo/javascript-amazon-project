1ef4e8d7684bdf150df5aae2d0cab52a8b45d997
jdosdo <jeremydosdo@gmail.com>
2024-07-29 13:05:37 +0700

    generate html for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,10 @@
-export const cart = [];
+export const cart = [{
+  productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+  quantity: 2
+}, {
+  productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+  quantity: 1
+}];
 
 export function addToCart(productId){
   let matchingItem;
@@ -8,7 +14,7 @@ export function addToCart(productId){
     // item it also has quantity. Check console.log(item);
     if(productId === cartItem.productId){
       matchingItem = cartItem; // if match, store it into a variable called matchingItem. This variable is an object, as 'item' is an object
-    }
+    };
   });
 
   if(matchingItem){
