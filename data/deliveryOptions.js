a3589dccd85f2f7833760972154ac0fec150c323
jdosdo <jeremydosdo@gmail.com>
2024-08-05 15:42:58 +0700

    save data and generate html for payment summary

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -10,4 +10,19 @@ export const deliveryOptions = [{
   id: '3',
   deliveryDays: 1,
   priceCents: 999
-}]
+}];
+
+export function getDeliveryOption(deliveryOptionId){
+  let deliveryOption;
+
+  deliveryOptions.forEach((option) => {
+    if(option.id === deliveryOptionId){
+      deliveryOption = option;
+    };
+  });
+
+  return deliveryOption || deliveryOption[0];
+  // to be save we give deliveryOption a default value to return
+  // which is deliveryOption[0]. Which is an array of delivery option
+  // [0] means first index, which is in this case is id: '1'
+}
