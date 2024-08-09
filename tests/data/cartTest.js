8e455d34736462411e38ee0219a35878d5022828
jdosdo <jeremydosdo@gmail.com>
2024-08-09 12:20:10 +0700

    add test checking setItem recieved correct values, and create beforeEach hooks

--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -1,9 +1,11 @@
 import { cart, addToCart, loadFromStorage } from '../../data/cart.js'
 
 describe('test suite: addToCart', () => {
-  it('adds an existing product to the cart', () => {
+  beforeEach(() => {
     spyOn(localStorage, 'setItem');
+  });
 
+  it('adds an existing product to the cart', () => {
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([{
         productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
@@ -17,14 +19,17 @@ describe('test suite: addToCart', () => {
     addToCart('15b6fc6f-327a-4ec4-896f-486349e85a3d', 1);
     expect(cart.length).toEqual(1);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
-    expect(cart[0].productId).toEqual('15b6fc6f-327a-4ec4-896f-486349e85a3d')
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 2,
+      deliveryOptionId: '1'
+    }])); //THIS IS EXERCISE 16C
+    expect(cart[0].productId).toEqual('15b6fc6f-327a-4ec4-896f-486349e85a3d');
     expect(cart[0].quantity).toEqual(2);
-
+    
   });
 
   it('adds a new product to the cart', () => {
-    spyOn(localStorage, 'setItem');
-
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([]);
     });
@@ -34,6 +39,11 @@ describe('test suite: addToCart', () => {
     addToCart('15b6fc6f-327a-4ec4-896f-486349e85a3d', 1);
     expect(cart.length).toEqual(1);
     expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+      productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+      quantity: 1,
+      deliveryOptionId: '1'
+    }])); // THIS IS EXERCISE 16D
     expect(cart[0].productId).toEqual('15b6fc6f-327a-4ec4-896f-486349e85a3d')
     expect(cart[0].quantity).toEqual(1);
   });
